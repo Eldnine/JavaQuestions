e9c16183012b4df450ecd803e79a8629419da080
Liao Yiyang <liso.yiy@gmail.com>
2016-01-05 15:42:19 +0800

    Create narcissisticNumber.java

---
+++ narcissisticNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class narcissisticNumber {
+	public static void main(String[] args) {
+		int b1,b2,b3,n;
+		for (b1=1;b1<10;b1++){
+			for (b2=1;b2<10;b2++){
+				for (b3=1;b3<10;b3++){
+					n=b1+b2*10+b3*100;
+					if(n==(Math.pow(b1, 3)+Math.pow(b2, 3)+Math.pow(b3, 3))){
+						System.out.println(n);
+					}
+				}
+			}
+		}
+	}
+}
