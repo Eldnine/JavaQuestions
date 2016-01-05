84919deca680e5aadf8933c8ae6ae62fa17eff4b
Liao Yiyang <liso.yiy@gmail.com>
2016-01-05 15:43:44 +0800

    Update narcissisticNumber.java

--- narcissisticNumber.java
+++ narcissisticNumber.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class narcissisticNumber {
 	public static void main(String[] args) {
 		int b1,b2,b3,n;
-		for (b1=1;b1<10;b1++){
-			for (b2=1;b2<10;b2++){
+		for (b1=0;b1<10;b1++){
+			for (b2=0;b2<10;b2++){
 				for (b3=1;b3<10;b3++){
 					n=b1+b2*10+b3*100;
 					if(n==(Math.pow(b1, 3)+Math.pow(b2, 3)+Math.pow(b3, 3))){
