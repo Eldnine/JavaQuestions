7339c4518044b4950da25360badfa5e922d1273c
Liao Yiyang <liso.yiy@gmail.com>
2016-01-07 19:31:31 +0800

    Create IsPermutation.java
    Given two strings, write a method to decide if one is a permutation of the other.
    Assume that it's case sensitive and the whitespace is significant

---
+++ IsPermutation.java
@@ -0,0 +1,23 @@
+//Given two strings, write a method to decide if one is a permutation of the other.
+//Assume that it's case sensitive and the whitespace is significant
+import java.util.*;
+public class IsPermutation {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		String s1=scanner.nextLine();
+		String s2=scanner.nextLine();
+		if(permutation(s1,s2)) System.out.println("Yes, they are permutation");
+		else System.out.println("No, they are not permutaion");
+	}
+	public static boolean permutation (String s1, String s2){
+		if(s1.length()!=s2.length()) return false;
+		char[] s1Char=s1.toCharArray();
+		char[] s2Char=s2.toCharArray();
+		java.util.Arrays.sort(s1Char);
+		java.util.Arrays.sort(s2Char);
+		s1=new String(s1Char);
+		s2=new String(s2Char);
+		return s1.equals(s2);
+	}
+}
+
